9d8a6b48bbd315168f55f9c11cae08f9d47e6ebe
Estuardo Icú <estuardo.icu@gmail.com>
2020-02-22 14:08:12 -0600

    feat: Registro y Autenticación
    Se agrega la funcionalidad de registrar un nuevo usuario y la autenticación de usuario para el manejo de sesiones.

--- proyecto/src/app/login/login.page.ts
+++ proyecto/src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from  "@angular/router";
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +10,25 @@ import { Router } from  "@angular/router";
 })
 export class LoginPage implements OnInit {
 
-  constructor(private  router:  Router) { }
+  constructor(private authService: AuthService, 
+              public router: Router,
+              public toastController: ToastController) { }
 
   ngOnInit() {
   }
-  
-  login(form){
-      this.router.navigateByUrl('home');
+
+  login(form) {
+    this.authService.login(form.value.email, form.value.pass).then( res =>{
+      this.router.navigate(['/home']);
+    }).catch(err => this.presentToast('los datos son incorrectos o no existe el usuario'))
   }
+
+  async presentToast(mensaje:string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    });
+    toast.present();
+  }
+
 }
---
+++ proyecto/src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    nombre: string;
+    email: string;
+    pass: string;
+    fecha: string;
+    direccion: string;
+    tipo: string
+}
