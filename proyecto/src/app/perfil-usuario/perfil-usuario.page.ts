3a6ebe311deacb13b824b3b2fc6dcaff2809c182
José Véliz <CiberVeliz97@gmail.com>
2020-04-02 08:00:13 -0600

    Cambios del usuario persistentes

--- proyecto/src/app/perfil-usuario/perfil-usuario.page.ts
+++ proyecto/src/app/perfil-usuario/perfil-usuario.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+
 import { User } from '../models/user';
 import { Router } from '@angular/router';
 import { StorageService } from '../storage.service'
+import { ApiService } from '../services/api.service'
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -14,7 +16,10 @@ export class PerfilUsuarioPage implements OnInit {
   Usuario:User;
   dataReady = false;
 
-  constructor(private router: Router, private storageService: StorageService) { }
+  constructor(
+    private router: Router, 
+    private storageService: StorageService,
+    private servicio: ApiService) { }
   
   ngOnInit() {
     this.storageService.getCurrentUser().then(result => {
@@ -22,22 +27,36 @@ export class PerfilUsuarioPage implements OnInit {
       
       this.Usuario = result;
       console.log('Fecha:'+this.Usuario.fecha_nacimiento);
+      console.log(this.Usuario)
       this.dataReady = true;
     })
   }
 
-  logout(){
+  logout()
+  {
     this.storageService.remove("usuario")
     this.router.navigate(["/login"]);
   }
 
-  guardar(modif:User){
+  guardar(modif:User)
+  {
     console.log('Funcion para guardar informacion');
-      //primero elimino informacion
-      this.storageService.remove('usuario');
-      console.log('modificado: '+modif);
-      this.storageService.setObject('usuario',modif);
+    //primero elimino informacion
+    
+    this.storageService.remove('usuario');
+    console.log('modificado: '+modif);
+    this.storageService.setObject('usuario', modif);
+    
+    let data = {
+      nombre: modif.nombre,
+      contra: modif.pass,
+      fecha_nacimiento: modif.fecha_nacimiento,
+      direccion: modif.direccion,
+      codigo: modif.codigo
+    }
 
+    console.log(data)
+    this.servicio.executePost('https://gzmqm82c19.execute-api.us-east-1.amazonaws.com/gtec/update-usuario', data)
   }
 
 }
